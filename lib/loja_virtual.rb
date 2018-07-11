a7059eeeedb37366d28f16c5f22cff5651542e99
bergpb <lindembergdepaulo@gmail.com>
2018-07-11 11:59:08 -0300

    corrigindo o erro do mixings nos requires no arquivo loja virtual

--- lib/loja_virtual.rb
+++ lib/loja_virtual.rb
@@ -1,8 +1,8 @@
+require File.expand_path('lib/formatador_moeda') # deve ser um dos primeiros no include, pois e usado na classe livros
 require File.expand_path('lib/midia')
 require File.expand_path('lib/dvd')
 require File.expand_path('lib/livro')
 require File.expand_path('lib/biblioteca')
 require File.expand_path('lib/relatorio')
 require File.expand_path('lib/banco_de_arquivos')
-require File.expand_path('lib/formatador_moeda')
 
